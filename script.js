1b4f0c867fe61dcc44df69725fc8069bd1a1c57c
Shailja Patil <shailja2908@gmail.com>
2024-04-10 00:21:22 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenue(){
+    const menue= document.querySelector(".menue-links");
+    const icon= document.querySelector(".hamburger-icon");
+    menue.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
